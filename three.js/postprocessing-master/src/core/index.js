4ed57057ae8b721e592685f1c6af5f4b30c0f8f2
a55663340 <a6232241@g.pccu.edu.tw>
2019-06-26 21:51:42 +0800

    移除postprocessing-master新增shader-physical及shadowmap

--- three.js/postprocessing-master/src/core/index.js
+++
@@ -1,4 +0,0 @@
-export { Disposable } from "./Disposable.js";
-export { Initializable } from "./Initializable.js";
-export { EffectComposer } from "./EffectComposer.js";
-export { Resizable } from "./Resizable.js";
